aa436d1eff42bcff2ab72e7f82f4b11ad3c3b3b0
nemanja233 <neca.nemanja.2007@gmail.com>
2026-05-07 23:23:03 +0200

    Visualized the scrolling

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,48 @@ static const unsigned char image_stats_bits[] U8X8_PROGMEM = {0x00,0x00,0x01,0x0
 
 
 
+// [BEGIN lopaka generated]
+static const unsigned char image_music_next_bits[] U8X8_PROGMEM = {0x00,0x00,0x07,0xf0,0x09,0x90,0x31,0x90,0x41,0x90,0x81,0x91,0x01,0x92,0x01,0x9c,0x01,0x9c,0x01,0x92,0x81,0x91,0x41,0x90,0x31,0x90,0x09,0x90,0x07,0xf0,0x00,0x00};
+static const unsigned char image_music_play_button_bits[] U8X8_PROGMEM = {0xe0,0x03,0x18,0x0c,0x04,0x10,0x02,0x20,0x32,0x20,0xd1,0x41,0x11,0x46,0x11,0x58,0x11,0x46,0xd1,0x41,0x32,0x20,0x02,0x20,0x04,0x10,0x18,0x0c,0xe0,0x03,0x00,0x00};
+static const unsigned char image_music_previous_bits[] U8X8_PROGMEM = {0x00,0x00,0x0f,0xe0,0x09,0x90,0x09,0x8c,0x09,0x82,0x89,0x81,0x49,0x80,0x39,0x80,0x39,0x80,0x49,0x80,0x89,0x81,0x09,0x82,0x09,0x8c,0x09,0x90,0x0f,0xe0,0x00,0x00};
+
+
+void drawMusic_Screen(void) {
+    u8g2.setFontMode(1);
+    u8g2.setBitmapMode(1);
+    u8g2.drawXBMP(55, 44, 15, 16, image_music_play_button_bits);
+
+    u8g2.drawXBMP(90, 44, 16, 16, image_music_next_bits);
+
+    u8g2.drawXBMP(19, 44, 16, 16, image_music_previous_bits);
+
+    u8g2.sendBuffer();
+}
+// [END lopaka generated]
+
+
+
 #define CLK_PIN 21
 #define DT_PIN 20
 #define CONFIRM_PIN 0   // Encoder button
-#define TOUCH_PIN 5
-#define HOME_PIN 6
+#define TOUCH_PIN 5     // Touch Sensor still no function
+#define RNDM_PIN 6      // Touch Sensor still no function
 #define SCL_PIN 9
 #define SDA_PIN 8
 
-const char* menuItems[] = {"Music", "Graph", "Timer", "Buddy"};
+
+struct MenuItem {
+  int x, y, w, h;
+  const unsigned char* icon;
+};
+
+MenuItem items[] = {
+  {  6, 4, 15, 16, image_music_headphones_bits },
+  { 38, 4, 14, 16, image_stats_bits            },
+  { 67, 4, 15, 16, image_clock_alarm_bits       },
+  { 93, 6, 29, 14, image_FaceNormal_bits        },
+};
+
 const int menuSize = 4;
 long oldPos = 0;
 int menuIndex = 0;
@@ -36,23 +69,54 @@ int buttonState = 0;
 
 Encoder myEnc(CLK_PIN, DT_PIN);
 
-void menuScreen(){
-  if (digitalRead(HOME_PIN) == HIGH){
-    delay(200);
-    u8g2.setFontMode(1);
-    u8g2.setBitmapMode(1);
-    u8g2.drawXBMP(6, 4, 15, 16, image_music_headphones_bits);
-    u8g2.drawXBMP(38, 4, 14, 16, image_stats_bits);
-    u8g2.drawXBMP(67, 4, 15, 16, image_clock_alarm_bits);
-    u8g2.drawXBMP(93, 6, 29, 14, image_FaceNormal_bits);
-    u8g2.sendBuffer();
-  }    
+void drawMenu() {
+  u8g2.clearBuffer();
+  u8g2.setFontMode(1);
+  u8g2.setBitmapMode(1);
+
+  for (int i = 0; i < menuSize; i++) {
+    MenuItem& m = items[i];
+    int pad = 4;
+    int bx = m.x - pad;
+    int by = m.y - pad;
+    int bw = m.w + pad * 2;
+    int bh = m.h + pad * 2;
+
+    if (i == menuIndex) {
+      // weißes Rechteck + Icon invertiert (schwarz auf weiß)
+      u8g2.setDrawColor(1);
+      u8g2.drawRBox(bx, by, bw, bh, 3);
+      u8g2.setDrawColor(2);   // XOR → invertiert das Icon
+      u8g2.drawXBMP(m.x, m.y, m.w, m.h, m.icon);
+    } else {
+      // nur Outline + normales Icon
+      u8g2.setDrawColor(1);
+      u8g2.drawRFrame(bx, by, bw, bh, 3);
+      u8g2.drawXBMP(m.x, m.y, m.w, m.h, m.icon);
+    }
+  }
+
+  u8g2.setDrawColor(1);  // zurücksetzen für nächste draws
+  u8g2.sendBuffer();
+}
+
+void onSelect(int i) {
+  u8g2.clearDisplay();
+  if(menuIndex == 0){
+    drawMusic_Screen();
+    if (digitalRead(TOUCH_PIN) == HIGH){
+      // TODO: NEXT, PREVIOS, PLAY/PAUSE functions
+    }
+  }
+  if(menuIndex == 1){
+    
+  }
 }
 
 
 void setup() {
   Serial.begin(115200);
-  pinMode(HOME_PIN, INPUT);
+  pinMode(RNDM_PIN, INPUT);
   pinMode(TOUCH_PIN, INPUT);
   pinMode(CONFIRM_PIN, INPUT_PULLUP);
   pinMode(CLK_PIN, INPUT_PULLUP);
@@ -60,10 +124,10 @@ void setup() {
 
   Wire.begin(SDA_PIN, SCL_PIN);
   u8g2.begin();
+  drawMenu();
 }
 
 void loop() {
-  menuScreen();
 
   long newPos = myEnc.read() / 2;   // 2 weil der Encoder 2 Schritte pro Klick macht
   if (newPos != oldPos){
@@ -81,11 +145,10 @@ void loop() {
       }
     }
     oldPos = newPos;
-    Serial.println(menuItems[menuIndex]);
+    drawMenu();
   }
   if (digitalRead(CONFIRM_PIN) == LOW){
-    delay(200);
-    Serial.println("Selected: " + String(menuItems[menuIndex]));
+    onSelect(menuIndex);
     delay(200);
   }
 
